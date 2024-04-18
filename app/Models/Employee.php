b22ea83fdb2f189ebb1750e965d4330b19d9b194
Jeison Manuyama <jeisonmanuyama@gmail.com>
2024-04-17 21:16:46 -0500

    models

---
+++ app/Models/Employee.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'surname', 'position', 'department_id', 'hire_date', 'salary'];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class);
+    }
+}
