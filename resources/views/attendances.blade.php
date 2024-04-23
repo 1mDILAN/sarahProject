2ea00d8261daaa6e214d9086b273997ee144d99a
Estudiantes <Estudiantes@cecep.edu.co>
2024-04-23 16:27:11 -0500

    ControllerNViews

---
+++ resources/views/attendances.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Attendances</title>
+</head>
+<body>
+    <h1>Attendances</h1>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+        </tr>
+    </table>
+</body>
+</html>
+
---
+++ resources/views/employees.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Employees</title>
+</head>
+<body>
+    <h1>Employees</h1>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+    </table>
+</body>
+</html>
